260c8bd633588a01728ca32aee4411a4aee16453
yoshiya0503 <myon53@gmail.com>
2017-04-13 14:18:49 +0900

    remove redundant code

--- src/routes.jsx
+++ src/routes.jsx
@@ -11,21 +11,6 @@ import Login from './containers/Login';
 import Profile from './containers/Profile';
 import Feed from './containers/Feed';
 
-class ProfileWrapper extends Component {
-  constructor() {
-    super();
-    this.user = {
-      id: 1,
-      name: 'yoshiya ito',
-      profile: '六畳一間の世界征服',
-      image: 'https://pbs.twimg.com/profile_images/755059325396094976/Vzvbd-6X_400x400.jpg',
-    };
-  }
-
-  render() {
-    return <Profile user={this.user} />;
-  }
-}
 
 class Routes extends Component {
 
@@ -34,7 +19,7 @@ class Routes extends Component {
       <Router history={hashHistory} >
         <Route path="/login" component={Login} />
         <Route path="/" component={Layout} >
-          <Route path="/profile" component={ProfileWrapper} />
+          <Route path="/profile" component={Profile} />
           <Route path="/feed" component={Feed} />
         </Route>
         <Route path="*" component={NotFound} />
